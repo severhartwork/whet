89808a4e340c27c201aa597979609717c1bd0ee0
Seth Everhart <setheverhartwork@gmail.com>
2022-01-30 20:06:04 -0500

    closing down for PS vids

---
+++ whet/js/index.js
@@ -0,0 +1,13 @@
+// index.js
+
+let hasJob = true;
+if (hasJob) {
+    document.writeln("Thanks for visiting");
+}
+
+else {
+    document.writeln("Please look around");
+}
+
+let today = new Date();
+let dayOfWeek = today.getDay();
